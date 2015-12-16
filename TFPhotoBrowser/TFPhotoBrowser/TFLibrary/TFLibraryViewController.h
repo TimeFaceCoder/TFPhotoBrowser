8a54ab54cb4d0dc55dd067ac92dc7e7216d50163
Melvin <m.yang0204@gmail.com>
2015-12-16 15:04:46 +0800

    init

---
+++ TFPhotoBrowser/TFPhotoBrowser/TFLibrary/TFLibraryViewController.h
@@ -0,0 +1,13 @@
+//
+//  TFLibraryViewController.h
+//  TFPhotoBrowser
+//
+//  Created by Melvin on 12/15/15.
+//  Copyright © 2015 TimeFace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFLibraryViewController : UIViewController
+
+@end
